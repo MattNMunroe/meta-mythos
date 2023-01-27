c335545af62b2f908a8b2e09b7c32f50f3a43c0a
Matt M <Afrodesiacc@gmail.com>
2023-01-27 11:29:29 -0500

    Added Componenets. Added About, Home, and Footer pages. Added Routes to App.js. No backend work yet.

---
+++ frontend/meta-mythos/src/Components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const About = () => {
+    return (
+        <div>
+            <h3>The history of humanity is one rife with creativity (amongst other things). Natural phenomenon were observed around us, and before there was the technology to explain it, people simply had their five senses and the Void of Mind to help them put the pieces together.
+This lead to an explation of 'Neteru' or 'Nature' using grandiose and exquiste imagery of gods and creatures alike. From Anansi to Ziz, Meta-Mythos is a hub for those looking to learn more about our web of consciousness through studying the ideas of the past, while simultaneously giving the user a vast worldy knowledge of mythic creatures and figures, which is sure to make you the life of any party! 
+Delve into the Myth, today!</h3>
+        </div>
+    )
+}
+
+export default About
---
+++ frontend/meta-mythos/src/Components/Home.js
@@ -0,0 +1,16 @@
+//Home page with title
+//Const NavBar
+//Slideshow of uncopyrighted pictures of mythological beings/creatures
+//Search Bar
+
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Meta-Mythos!</h1>
+        </div>
+    )
+}
+
+export default Home
