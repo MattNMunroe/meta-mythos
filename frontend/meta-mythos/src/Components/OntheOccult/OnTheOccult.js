5d72d394680c8283d8ba4dcb090c6fdd53bc77d8
Matt M <Afrodesiacc@gmail.com>
2023-02-07 21:12:38 -0500

    Began to explain the history of the occult.

---
+++ frontend/meta-mythos/src/Components/OntheOccult/OnTheOccult.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const OnTheOccult = () => {
+    return (
+        <div>
+            <h2>FAQs, and other inquries</h2>
+            <break></break>
+            <h3>Just what is the "Occult"? What's it mean, and stand for?</h3>
+            <p>This word, when heard by many, strikes a soert of metaphysical fear into their spiritual well being. Oft times one has heard "Oh, I don't mess with that." or "Nah, man that's evil."</p>
+            <p>"Occult" simply means "Hidden". Hidden Knowledge, to be exact. Elements of psychology and mysticism that is used to explain natural phenomenon."</p>
+        </div>
+    )
+}
+
+export default OnTheOccult
