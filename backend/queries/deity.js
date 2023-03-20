64854e6011f747a55b8b7b55605b9ae426c1fa75
Matt M <Afrodesiacc@gmail.com>
2023-03-20 11:04:33 -0400

    Updated deity query.

--- backend/queries/deity.js
+++ backend/queries/deity.js
@@ -1,10 +1,65 @@
-const db = require("../db/dbConfig")
+const db = require("../db/dbConfig");
 
 const getAllDeities = async () => {
-    try {
-        const allDeities = await db.any("SELECT * FROM myths");
-        return allDeities
-    } catch (error) {
-        return error;
-    }
+  try {
+    const allDeities = await db.any("SELECT * FROM myths");
+    return allDeities;
+  } catch (error) {
+    return error;
+  }
+};
+
+const getDeity = async (id) => {
+  try {
+    const oneDeity = await db.one("SELECT * FROM myths WHERE id=$1", id);
+    return oneDeity;
+  } catch (error) {
+    return error;
+  }
+};
+
+const deleteDeity = async (id) => {
+  try {
+    const oneDeity = await db.one(
+      "DELETE FROM users WHERE id=$1 RETURNING *",
+      id
+    );
+    return oneDeity;
+  } catch (error) {
+    console.log(error.message || error);
+    return error;
+  }
+};
+
+const createDeity = async ({ name, description, tradition, reference }) => {
+  try {
+    const newDeity = await db.one(
+      "INSERT INTO myths (name, description, tradition, reference) VALUES($1, $2. $3, $4) RETURNING *",
+      [name, description, tradition, reference]
+    );
+    return newDeity;
+  } catch (error) {
+    return error;
+  }
+};
+
+const updateDeity = async (id, { name, description, tradition, reference }) => {
+  try {
+    const updateDeity = await db.one(
+      "UPDATE myths SET name=$1, description=$2, tradition=$3, reference=$4 where id=$5 RETURNING *",
+      [name, description, tradition, reference]
+    );
+    return updateDeity;
+  } catch (error) {
+    console.log(error.message || error);
+    return error;
+  }
+};
+
+module.exports = {
+  getAllDeities,
+  getDeity,
+  deleteDeity,
+  createDeity,
+  updateDeity,
 };
